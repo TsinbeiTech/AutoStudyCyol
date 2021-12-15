4f81f34cf6fa6b2cb68321b390ed60f84017f213
TsinbeiTech <team@tsinbei.com>
2021-12-15 17:38:52 +0800

    Update and rename send_module/server_chan/sender.py to send_module/go_cqhttp/sender.py

---
+++ send_module/go_cqhttp/sender.py
@@ -0,0 +1,312 @@
+import requests
+
+sess = requests.session()
+user_id = ""
+access_token = ""
+api_url = ""
+group_id = ""
+at_user = ""
+
+def set_api_url(url):
+    global api_url
+    api_url = url
+
+def set_access_token(token):
+    global access_token
+    access_token = token
+
+def set_group_id(id):
+    global group_id
+    group_id = id
+    
+def set_at_user(user):
+    global at_user
+    at_user = user
+    
+def set_user_id(id):
+    global user_id
+    user_id = id
+
+def send(title, content) -> dict:
+    global at_user
+    empty = group_id == ""
+    noat = at_user == ""
+    if noat and empty:
+                     at_user = user_id
+    if empty:
+            resp = sess.post(url=f"{api_url}/send_private_msg?access_token={access_token}", data={
+                'user_id': f"{user_id}",
+                'message': f"{title}\n\n{content}"
+            })
+    else:
+        resp = sess.post(url=f"{api_url}/send_group_msg?access_token={access_token}", data={
+                'group_id': f"{group_id}",
+                'message': f"[CQ:at,qq={user_id}]\n\n{title}\n\n{content}" if not noat else f"{title}\n\n{content}"
+            })
+    res = resp.json()
+    success = res['status'] == "ok"
+    return {
+        'success': success,
+        'message': res['msg'] if not success else '发送成功'
+    }
+
+import requests
+
+sess = requests.session()
+user_id = ""
+access_token = ""
+api_url = ""
+group_id = ""
+at_user = ""
+
+def set_api_url(url):
+    global api_url
+    api_url = url
+
+def set_access_token(token):
+    global access_token
+    access_token = token
+
+def set_group_id(id):
+    global group_id
+    group_id = id
+    
+def set_at_user(user):
+    global at_user
+    at_user = user
+    
+def set_user_id(id):
+    global user_id
+    user_id = id
+
+def send(title, content) -> dict:
+    global at_user
+    empty = group_id == ""
+    noat = at_user == ""
+    if noat and empty:
+                     at_user = user_id
+    if empty:
+            resp = sess.post(url=f"{api_url}/send_private_msg?access_token={access_token}", data={
+                'user_id': f"{user_id}",
+                'message': f"{title}\n\n{content}"
+            })
+    else:
+        resp = sess.post(url=f"{api_url}/send_group_msg?access_token={access_token}", data={
+                'group_id': f"{group_id}",
+                'message': f"[CQ:at,qq={user_id}]\n\n{title}\n\n{content}" if not noat else f"{title}\n\n{content}"
+            })
+    res = resp.json()
+    success = res['status'] == "ok"
+    return {
+        'success': success,
+        'message': res['msg'] if not success else '发送成功'
+    }
+
+import requests
+
+sess = requests.session()
+user_id = ""
+access_token = ""
+api_url = ""
+group_id = ""
+at_user = ""
+
+def set_api_url(url):
+    global api_url
+    api_url = url
+
+def set_access_token(token):
+    global access_token
+    access_token = token
+
+def set_group_id(id):
+    global group_id
+    group_id = id
+    
+def set_at_user(user):
+    global at_user
+    at_user = user
+    
+def set_user_id(id):
+    global user_id
+    user_id = id
+
+def send(title, content) -> dict:
+    global at_user
+    empty = group_id == ""
+    noat = at_user == ""
+    if noat and empty:
+                     at_user = user_id
+    if empty:
+            resp = sess.post(url=f"{api_url}/send_private_msg?access_token={access_token}", data={
+                'user_id': f"{user_id}",
+                'message': f"{title}\n\n{content}"
+            })
+    else:
+        resp = sess.post(url=f"{api_url}/send_group_msg?access_token={access_token}", data={
+                'group_id': f"{group_id}",
+                'message': f"[CQ:at,qq={user_id}]\n\n{title}\n\n{content}" if not noat else f"{title}\n\n{content}"
+            })
+    res = resp.json()
+    success = res['status'] == "ok"
+    return {
+        'success': success,
+        'message': res['msg'] if not success else '发送成功'
+    }
+
+import requests
+
+sess = requests.session()
+user_id = ""
+access_token = ""
+api_url = ""
+group_id = ""
+at_user = ""
+
+def set_api_url(url):
+    global api_url
+    api_url = url
+
+def set_access_token(token):
+    global access_token
+    access_token = token
+
+def set_group_id(id):
+    global group_id
+    group_id = id
+    
+def set_at_user(user):
+    global at_user
+    at_user = user
+    
+def set_user_id(id):
+    global user_id
+    user_id = id
+
+def send(title, content) -> dict:
+    global at_user
+    empty = group_id == ""
+    noat = at_user == ""
+    if noat and empty:
+                     at_user = user_id
+    if empty:
+            resp = sess.post(url=f"{api_url}/send_private_msg?access_token={access_token}", data={
+                'user_id': f"{user_id}",
+                'message': f"{title}\n\n{content}"
+            })
+    else:
+        resp = sess.post(url=f"{api_url}/send_group_msg?access_token={access_token}", data={
+                'group_id': f"{group_id}",
+                'message': f"[CQ:at,qq={user_id}]\n\n{title}\n\n{content}" if not noat else f"{title}\n\n{content}"
+            })
+    res = resp.json()
+    success = res['status'] == "ok"
+    return {
+        'success': success,
+        'message': res['msg'] if not success else '发送成功'
+    }
+
+import requests
+
+sess = requests.session()
+user_id = ""
+access_token = ""
+api_url = ""
+group_id = ""
+at_user = ""
+
+def set_api_url(url):
+    global api_url
+    api_url = url
+
+def set_access_token(token):
+    global access_token
+    access_token = token
+
+def set_group_id(id):
+    global group_id
+    group_id = id
+    
+def set_at_user(user):
+    global at_user
+    at_user = user
+    
+def set_user_id(id):
+    global user_id
+    user_id = id
+
+def send(title, content) -> dict:
+    global at_user
+    empty = group_id == ""
+    noat = at_user == ""
+    if noat and empty:
+                     at_user = user_id
+    if empty:
+            resp = sess.post(url=f"{api_url}/send_private_msg?access_token={access_token}", data={
+                'user_id': f"{user_id}",
+                'message': f"{title}\n\n{content}"
+            })
+    else:
+        resp = sess.post(url=f"{api_url}/send_group_msg?access_token={access_token}", data={
+                'group_id': f"{group_id}",
+                'message': f"[CQ:at,qq={user_id}]\n\n{title}\n\n{content}" if not noat else f"{title}\n\n{content}"
+            })
+    res = resp.json()
+    success = res['status'] == "ok"
+    return {
+        'success': success,
+        'message': res['msg'] if not success else '发送成功'
+    }
+
+import requests
+
+sess = requests.session()
+user_id = ""
+access_token = ""
+api_url = ""
+group_id = ""
+at_user = ""
+
+def set_api_url(url):
+    global api_url
+    api_url = url
+
+def set_access_token(token):
+    global access_token
+    access_token = token
+
+def set_group_id(id):
+    global group_id
+    group_id = id
+    
+def set_at_user(user):
+    global at_user
+    at_user = user
+    
+def set_user_id(id):
+    global user_id
+    user_id = id
+
+def send(title, content) -> dict:
+    global at_user
+    empty = group_id == ""
+    noat = at_user == ""
+    if noat and empty:
+                     at_user = user_id
+    if empty:
+            resp = sess.post(url=f"{api_url}/send_private_msg?access_token={access_token}", data={
+                'user_id': f"{user_id}",
+                'message': f"{title}\n\n{content}"
+            })
+    else:
+        resp = sess.post(url=f"{api_url}/send_group_msg?access_token={access_token}", data={
+                'group_id': f"{group_id}",
+                'message': f"[CQ:at,qq={user_id}]\n\n{title}\n\n{content}" if not noat else f"{title}\n\n{content}"
+            })
+    res = resp.json()
+    success = res['status'] == "ok"
+    return {
+        'success': success,
+        'message': res['msg'] if not success else '发送成功'
+    }
+
